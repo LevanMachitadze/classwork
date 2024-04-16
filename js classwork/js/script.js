10bfa2753c8385d477b98743da7dc448e75dcae0
LevanMachitadze <160760563+LevanMachitadze@users.noreply.github.com>
2024-04-16 20:54:35 +0400

    Add files via upload

---
+++ js classwork/js/script.js
@@ -0,0 +1,24 @@
+let answer = '';
+answer = prompt('შეიყვანეთ სიტყვა');
+
+if (answer.toLowerCase()[0] === 'a') {
+  alert(`თქვენი სიტყვა იწყება ხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLowerCase()[0] === 'e') {
+  alert(`თქვენი სიტყვა იწყება ხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLowerCase()[0] === 'i') {
+  alert(`თქვენი სიტყვა იწყება ხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLowerCase()[0] === 'o') {
+  alert(`თქვენი სიტყვა იწყება ხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLowerCase()[0] === 'u') {
+  alert(`თქვენი სიტყვა იწყება ხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLocaleLowerCase()[0] !== 'a') {
+  alert(`თქვენი სიტყვა იწყება თანხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLocaleLowerCase()[0] !== 'e') {
+  alert(`თქვენი სიტყვა იწყება თანხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLocaleLowerCase()[0] !== 'i') {
+  alert(`თქვენი სიტყვა იწყება თანხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLocaleLowerCase()[0] !== 'o') {
+  alert(`თქვენი სიტყვა იწყება თანხმოვნით, რომელიც არის ${answer[0]}`);
+} else if (answer.toLocaleLowerCase()[0] !== 'u') {
+  alert(`თქვენი სიტყვა იწყება თანხმოვნით, რომელიც არის ${answer[0]}`);
+} else 'ERORR!!!';
